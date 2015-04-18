1e592c414607b3bdb7f80f87f8856c60924565df
tonchan1216 <viva.la.vida27315@gmail.com>
2015-04-18 23:09:14 +0900

    wpの準備

--- wp/footer.php
+++ wp/footer.php
@@ -1,4 +1,5 @@
   <!-- フッター -->
+
   <div id="footer">
     <div class="inner">
       <!-- 3カラム -->
@@ -6,40 +7,41 @@
       <section class="gridWrapper">
         <article class="grid">
           <!-- ロゴ -->
-
-          <p class="logo"><a href="index.html">会社名・ロゴ<br>
-          <span>キャッチコピーが入ります。</span></a></p><!-- / ロゴ -->
+          <p class="logo">
+            <a href="<?php bloginfo('url'); ?>"><img class="logoImg" src="<?php echo get_template_directory_uri(); ?>/images/logo.png"><br>
+            <span>東北大学学友会交響楽団</span></a>
+          </p><!-- / ロゴ -->
         </article>
 
         <article class="grid">
           <ul class="list-inline text-a-c">
             <li>
-              <a href="sitemap.html">サイトマップ</a>
+              <a href="<?php bloginfo('url'); ?>sitemap.html">サイトマップ</a>
             </li>
 
             <li>
-              <a href="privacypolicy.html">プライバシーポリシー</a>
+              <a href="<?php bloginfo('url'); ?>/privacypolicy.html">プライバシーポリシー</a>
             </li>
 
             <li>
-              <a href="idemnity.html">免責事項</a>
+              <a href="<?php bloginfo('url'); ?>/idemnity.html">免責事項</a>
             </li>
 
             <li>
-              <a href="menber.html">団員専用ページへ</a>
+              <a href="<?php bloginfo('url'); ?>/member.html">団員専用ページへ</a>
             </li>
           </ul>
         </article>
 
-        <article class="f-right grid text-a-r">
+        <article class="grid f-right text-a-r">
           <!-- 電話番号+受付時間 -->
 
-          <p class="tel">電話: <strong>0000-0000-0000</strong></p>
+          <p class="mail">E-mail: <strong>abcde@ABC.com</strong></p>
 
           <p><a href="http://validator.w3.org/check?uri=referer"><img alt="Valid XHTML 1.0 Transitional" height="31" src="http://www.w3.org/Icons/valid-xhtml10" width="88"></a> <a href="http://jigsaw.w3.org/css-validator/"><img alt="Valid CSS!" src="http://jigsaw.w3.org/css-validator/images/vcss" style="border:0;width:88px;height:31px"></a></p>
 
           <p><!-- FC2カウンター ここから --><script language="JavaScript" src="http://counter1.fc2.com/counter.php?id=2160762" type="text/javascript"></script> <noscript><img alt="counter" src="http://counter1.fc2.com/counter_img.php?id=2160762"><br>
-          <strong><a href="http://flowerfan.com/">ギフト</a></strong></noscript> <!-- FC2カウンター ここまで --></p><!-- / 電話番号+受付時間 -->
+          <strong><a href="http://flowerfan.com/">ギフト</a></strong></noscript> <!-- FC2カウンター ここまで --></p>
         </article>
       </section><!-- / 3カラム -->
     </div>
@@ -48,13 +50,14 @@
       (C) Copyright 東北大学交響楽団／東北大学学友会交響楽部　2015 All rights reserved.
     </div>
   </div><!-- / フッター -->
-  <script src="js/jquery1.7.2.min.js"></script> 
-  <script src="js/script.js"></script> 
-  <script src="js/jquery.bxslider.js"></script> 
+  <script src="<?php echo get_template_directory_uri(); ?>/js/jquery1.7.2.min.js"></script> 
+  <script src="<?php echo get_template_directory_uri(); ?>/js/script.js"></script> 
+  <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.bxslider.js"></script> 
   <script>
     $(function(){
       $('.bxslider').bxSlider({
-        auto: true
+        auto: true,
+        mode: 'fade'
       });
     });
   </script>
--- wp/header.php
+++ wp/header.php
@@ -8,21 +8,68 @@
   <meta content="東北,大学,学友会,交響楽,オケ,オーケストラ" name="keywords">
 
   <title>Tohoku University Symphony Orchestra</title>
-  <link href="style.css" media="screen" rel="stylesheet" type="text/css">
-  <link href="css/jquery.bxslider.css" rel="stylesheet" type="text/css"><!--[if lt IE 9]>
+  <link href="<?php echo get_template_directory_uri(); ?>/style.css" media="screen" rel="stylesheet" type="text/css">
+  <link href="<?php echo get_template_directory_uri(); ?>/css/jquery.bxslider.css" rel="stylesheet" type="text/css"><!--[if lt IE 9]>
   <script src="js/html5.js"></script>
   <script src="js/css3-mediaqueries.js"></script>
   <![endif]-->
-  <?php wp_head();?>
+  <?php wp_footer(); ?>
+
 </head>
 
 <body>
   <!-- ヘッダー -->
 
   <div id="header">
-    <div class="inner">
+    <div class="fullwrapper">
 
       <!-- ロゴ -->
       <div class="logo">
-        <a href="index.html"><img src="images/title.png"></a>
+        <a href="<?php bloginfo('url'); ?>"><img class="logoImg" src="<?php echo get_template_directory_uri(); ?>/images/logo.png"><img src="<?php echo get_template_directory_uri(); ?>/images/title.png"></a>
       </div><!-- / ロゴ -->
+
+      <!-- メインナビゲーション -->
+      <nav id="mainNav">
+        <a class="menu" id="menu"><span>MENU</span></a>
+
+        <div class="panel">
+          <ul>
+            <li>
+              <a href="<?php bloginfo('url'); ?>/news.html">ニュース<br>
+              <span>News</span></a>
+            </li>
+
+            <li>
+              <a href="<?php bloginfo('url'); ?>/concert.html">最新の演奏会<br>
+              <span>Concert</span></a>
+            </li>
+
+            <li>
+              <a href="<?php bloginfo('url'); ?>/pastconcert.html">過去の演奏会<br>
+              <span>Past Concert</span></a>
+            </li>
+
+            <li>
+              <a href="<?php bloginfo('url'); ?>/join.html">団員募集<br>
+              <span>Joinus</span></a>
+            </li>
+
+            <li>
+              <a href="<?php bloginfo('url'); ?>/about.html">当団について<br>
+              <span>About</span></a>
+            </li>
+
+            <li>
+              <a href="<?php bloginfo('url'); ?>/contact.html">お問い合わせ<br>
+              <span>Contact</span></a>
+            </li>
+
+            <li>
+              <a href="<?php bloginfo('url'); ?>/link.html">リンク<br>
+              <span>Link</span></a>
+            </li>
+          </ul>
+        </div>
+      </nav><!-- メインナビゲーション -->
+    </div>
+  </div><!-- / ヘッダー -->
