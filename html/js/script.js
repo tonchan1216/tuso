9ac84a60decca5c98130d4165a428e8abf055a61
tonchan1216 <viva.la.vida27315@gmail.com>
2015-04-26 19:46:19 +0900

    add page-up

--- html/js/script.js
+++ html/js/script.js
@@ -1,10 +1,26 @@
 $(function() {
-  $("div.panel").hide();
+ 	$("div.panel").hide();
   $(".menu").click(function() {
     $(this).toggleClass("menuOpen").next().slideToggle();
   });
+
   $(".slideList").hide();
   $(".readmore").click(function() {
   	$(".slideList").slideToggle(1000, "swing");
   });
+
+  $("#page-up").hide();
+  $(window).scroll(function() {
+  	if( $(this).scrollTop() > 60 ){
+			$("#page-up").fadeIn();
+		}else{
+			$("#page-up").fadeOut();
+		}
+  })
+  $("#page-up a").click(function() {
+      $("body").animate({
+        scrollTop:0
+      },500);
+      return false;
+  })
 })
