4d96a38819209b86645a77b0ff9db35295a00185
tonchan1216 <viva.la.vida27315@gmail.com>
2015-04-30 21:38:15 +0900

    bootstrap導入

--- html/js/script.js
+++ html/js/script.js
@@ -26,14 +26,38 @@ $(function() {
       return false;
   })
 
-  //tab-change
-  $('.tabbox:first').show();
-  $('#tab-menu li:first').addClass('active');
-  $('#tab-menu li').click(function() {
-    $('#tab-menu li').removeClass('active');
-    $(this).addClass('active');
-    $('.tabbox').hide();
-    $($(this).find('a').attr('href')).fadeIn();
-    return false;
+  btnPc = 'swPc';
+  btnSp = 'swSp';
+  btnAcvClass = 'btnAcv';
+  vpPc = 'width=1024';
+  vpSp = 'width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no';
+
+  // ViewPort
+  $('head').prepend('<meta name="viewport" content=' + ($.cookie('swView') == 1 ? vpPc : vpSp) + '>');
+  setBtn='#'+(btnPc)+',#'+(btnSp); setBtnPc='#'+(btnPc); setBtnSp='#'+(btnSp);
+  $(setBtn).on('click',function(){
+      $.cookie('swView', $(this).attr('id') == btnPc ? 1 : 0);
+      location.reload();
+      return false;
+  });
+
+  // BtnActive
+  var cookie = $.cookie('btnAcv');
+  if(cookie == 'acvPc'){
+      $(setBtnPc).addClass(btnAcvClass);
+      $(setBtnSp).removeClass(btnAcvClass);
+  } else if(cookie == 'acvSp'){
+      $(setBtnPc).removeClass(btnAcvClass);
+      $(setBtnSp).addClass(btnAcvClass);
+  }
+  $(setBtnPc).on('click',function(){
+      $.cookie('btnAcv','acvPc',{expires:7, path:'/'});
+      $(setBtnSp).removeClass(btnAcvClass);
+      $(this).addClass(btnAcvClass);
+  });
+  $(setBtnSp).on('click',function(){
+      $.cookie('btnAcv','acvSp',{expires:7, path:'/'});
+      $(setBtnPc).removeClass(btnAcvClass);
+      $(this).addClass(btnAcvClass);
   });
 })
