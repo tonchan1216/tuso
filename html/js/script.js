bf2d692747712415a1e52fc65b6b4f7876eec3b7
tonchan1216 <viva.la.vida27315@gmail.com>
2015-05-04 19:46:32 +0900

    change many .carousel .logo .image .nav bar

--- html/js/script.js
+++ html/js/script.js
@@ -21,39 +21,31 @@ $(function() {
     $(".slideList").slideToggle(1000,"swing");
   })
 
+  //view-port toggle
+	$(".view-toggle").click(function(){
+		status =  $(this).attr("id");
+		$.cookie("style",status, {expires:30});
+		vp_switch(status);
+		location.reload();
+		return false;
+	})
+		
+	if($.cookie("style")){
+		vp_switch($.cookie("style"));
+	}
+});
 
-  btnPc = 'swPc';
-  btnSp = 'swSp';
-  btnAcvClass = 'btnAcv';
-  vpPc = 'width=1024';
-  vpSp = 'width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no';
 
-  // ViewPort
-  $('head').prepend('<meta name="viewport" content=' + ($.cookie('swView') == 1 ? vpPc : vpSp) + '>');
-  setBtn='#'+(btnPc)+',#'+(btnSp); setBtnPc='#'+(btnPc); setBtnSp='#'+(btnSp);
-  $(setBtn).on('click',function(){
-      $.cookie('swView', $(this).attr('id') == btnPc ? 1 : 0);
-      location.reload();
-      return false;
-  });
-
-  // BtnActive
-  var cookie = $.cookie('btnAcv');
-  if(cookie == 'acvPc'){
-      $(setBtnPc).addClass(btnAcvClass);
-      $(setBtnSp).removeClass(btnAcvClass);
-  } else if(cookie == 'acvSp'){
-      $(setBtnPc).removeClass(btnAcvClass);
-      $(setBtnSp).addClass(btnAcvClass);
-  }
-  $(setBtnPc).on('click',function(){
-      $.cookie('btnAcv','acvPc',{expires:7, path:'/'});
-      $(setBtnSp).removeClass(btnAcvClass);
-      $(this).addClass(btnAcvClass);
-  });
-  $(setBtnSp).on('click',function(){
-      $.cookie('btnAcv','acvSp',{expires:7, path:'/'});
-      $(setBtnPc).removeClass(btnAcvClass);
-      $(this).addClass(btnAcvClass);
-  });
-})
+function vp_switch(s){
+	$("#"+s).addClass("active");
+	if(s=="btnSp"){
+	　　//  スマホなら
+		$('meta[name=viewport]').attr("content","width=480,initial-scale=1.6,minimum-scale=1.0");
+	}else if(s=="btnPc"){
+	　　//  PCなら
+		$('meta[name=viewport]').attr("content","width=960,initial-scale=0.7,minimum-scale=0.25");
+	}else{
+	　　//  どちらでもない場合
+		$('meta[name=viewport]').attr("content","width=device-width,initial-scale=1.0,minimum-scale=1.0");
+	}
+}
